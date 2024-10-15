46b746648a7e360f6a8cf968fd1ef030a37d0efb
Gabriel Santos <gabrielsantospds@hotmail.com>
2024-10-14 23:37:16 -0300

    style: add position attribute to organize contact info

--- src/components/ComponentContactUs.vue
+++ src/components/ComponentContactUs.vue
@@ -9,19 +9,21 @@
 
             <div class="container__data-card">
                 <div class="data-card__item data-card__item--wide">
-                    <div class="data-card__icon-card">
+                    <div class="data-card__icon-card--wide">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
                             class="bi bi-telephone data-card__icon" viewBox="0 0 16 16">
                             <path
                                 d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
                         </svg>
                     </div>
-                    <h3 class="data-card__title">
-                        TELEFONE
-                    </h3>
-                    <h4 class="data-card__subtitle">
-                        21 3435-3534
-                    </h4>
+                    <div class="data-card__info-card--wide">
+                        <h3 class="data-card__title">
+                            TELEFONE
+                        </h3>
+                        <h4 class="data-card__subtitle">
+                            21 3435-3534
+                        </h4>
+                    </div>
                 </div>
                 <div class="data-card__item">
                     <div class="data-card__icon-card">
@@ -31,12 +33,14 @@
                                 d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
                         </svg>
                     </div>
-                    <h3 class="data-card__title">
-                        LEANDRO
-                    </h3>
-                    <h4 class="data-card__subtitle">
-                        21 3435-3534
-                    </h4>
+                    <div class="data-card__info-card">
+                        <h3 class="data-card__title">
+                            LEANDRO
+                        </h3>
+                        <h4 class="data-card__subtitle">
+                            21 3435-3534
+                        </h4>
+                    </div>
                 </div>
                 <div class="data-card__item">
                     <div class="data-card__icon-card">
@@ -46,12 +50,14 @@
                                 d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
                         </svg>
                     </div>
-                    <h3 class="data-card__title">
-                        SERGIO
-                    </h3>
-                    <h4 class="data-card__subtitle">
-                        21 3435-3534
-                    </h4>
+                    <div class="data-card__info-card">
+                        <h3 class="data-card__title">
+                            SERGIO
+                        </h3>
+                        <h4 class="data-card__subtitle">
+                            21 3435-3534
+                        </h4>
+                    </div>
                 </div>
                 <div class="data-card__item">
                     <div class="data-card__icon-card">
@@ -61,12 +67,14 @@
                                 d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
                         </svg>
                     </div>
-                    <h3 class="data-card__title">
-                        RENATA
-                    </h3>
-                    <h4 class="data-card__subtitle">
-                        21 3435-3534
-                    </h4>
+                    <div class="data-card__info-card">
+                        <h3 class="data-card__title">
+                            RENATA
+                        </h3>
+                        <h4 class="data-card__subtitle">
+                            21 3435-3534
+                        </h4>
+                    </div>
                 </div>
                 <div class="data-card__item">
                     <div class="data-card__icon-card">
@@ -76,15 +84,17 @@
                                 d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
                         </svg>
                     </div>
-                    <h3 class="data-card__title">
-                        LUCIANA
-                    </h3>
-                    <h4 class="data-card__subtitle">
-                        21 3435-3534
-                    </h4>
+                    <div class="data-card__info-card">
+                        <h3 class="data-card__title">
+                            LUCIANA
+                        </h3>
+                        <h4 class="data-card__subtitle">
+                            21 3435-3534
+                        </h4>
+                    </div>
                 </div>
                 <div class="data-card__item data-card__item--wide">
-                    <div class="data-card__icon-card">
+                    <div class="data-card__icon-card--wide">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
                             class="bi bi-geo-alt data-card__icon" viewBox="0 0 16 16">
                             <path
@@ -92,13 +102,15 @@
                             <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
                         </svg>
                     </div>
-                    <h3 class="data-card__title">
-                        ENDEREÇO
-                    </h3>
-                    <h4 class="data-card__subtitle">
-                        RUA NIVALDO GUERREIRO NUNES, 450 <br>
-                        DISTRITO INDUSTRIAL, UBERLÂNDIA MG
-                    </h4>
+                    <div class="data-card__info-card--wide">
+                        <h3 class="data-card__title">
+                            ENDEREÇO
+                        </h3>
+                        <h4 class="data-card__subtitle">
+                            RUA NIVALDO GUERREIRO NUNES, 450 <br>
+                            DISTRITO INDUSTRIAL, UBERLÂNDIA MG
+                        </h4>
+                    </div>
                 </div>
             </div>
         </div>
@@ -147,7 +159,7 @@
 .container__title {
     text-align: center;
     font-size: 60px;
-    margin: 40px 0 40px 0;
+    margin: 0 0 40px 0;
     text-shadow: 0 2px 2px var(--shadow-color);
 }
 
@@ -155,6 +167,7 @@
 
 .container--red {
     background-color: var(--primary-color);
+    padding: 4% 7%;
 }
 
 .container__title--white {
@@ -164,21 +177,18 @@
 .container__data-card {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    width: 64%;
-    margin: 50px auto;
     row-gap: 40px;
-    column-gap: 25%;
+    column-gap: 20%;
 }
 
 .data-card__item {
     color: white;
     display: grid;
-    grid-template-columns: 2fr 7fr;
+    grid-template-columns: 1fr 2fr;
 }
 
 .data-card__item--wide,
 .data-card__item--wide {
-    grid-template-columns: 0.8fr 7fr;
     grid-column-start: 1;
     grid-column-end: 3;
 }
@@ -186,13 +196,23 @@
 .data-card__icon-card {
     grid-row-start: 1;
     grid-row-end: 3;
-    margin-right: 12px;
+    position: relative;
+    left: 20%;
+}
+
+.data-card__icon-card--wide {
+    position: relative;
+    left: 8%;
+}
+
+.data-card__info-card--wide {
+    position: relative;
+    right: 30%
 }
 
 .data-card__title {
     font-size: 22px;
     text-shadow: 0 1px 2px var(--shadow-color);
-    padding-top: 4px;
 }
 
 .data-card__subtitle {
@@ -293,11 +313,6 @@
 
     /* container red style */
 
-    .container__data-card {
-        width: 69%;
-        column-gap: 22%;
-    }
-
     .data-card__icon {
         width: 32px;
         height: 32px;
@@ -333,23 +348,10 @@
         width: 100%;
     }
 
-    /* container red style */
-
-    .data-card__item--wide,
-    .data-card__item--wide {
-        grid-template-columns: 0.7fr 7fr;
-    }
-
 }
 
 @media screen and (min-width: 768px) and (max-width: 991px) {
 
-    /* container red style */
-
-    .container__data-card {
-        width: 62%;
-        column-gap: 30%;
-    }
 }
 
 @media screen and (min-width: 992px) and (max-width: 1199px) {
@@ -359,11 +361,6 @@
     }
 
     /* container red style */
-
-    .container__data-card {
-        width: 73%;
-        column-gap: 17%;
-    }
 
     .data-card__icon {
         width: 32px;
@@ -390,11 +387,5 @@
 
 @media screen and (min-width: 1200px) and (max-width: 1399px) {
 
-    /* container red style */
-    
-    .container__data-card {
-        width: 70%;
-        column-gap: 20%;
-    }
 }
 </style>
