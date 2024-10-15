4eebce08433135154aff9c7a225749ca7941676c
Gabriel Santos <gabrielsantospds@hotmail.com>
2024-10-14 22:33:26 -0300

    refactor: rearrenge style code

--- src/components/ComponentContactUs.vue
+++ src/components/ComponentContactUs.vue
@@ -7,95 +7,95 @@
                 Contatos
             </h1>
 
-            <div class="container-data">
-                <div class="content-data content-data-1">
-                    <div class="content-data-icon content-data-icon-1">
+            <div class="container__data-card">
+                <div class="data-card__item data-card__item--wide">
+                    <div class="data-card__icon-card">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
-                            class="bi bi-telephone icon-svg" viewBox="0 0 16 16">
+                            class="bi bi-telephone data-card__icon" viewBox="0 0 16 16">
                             <path
                                 d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
                         </svg>
                     </div>
-                    <h3 class="data-title">
+                    <h3 class="data-card__title">
                         TELEFONE
                     </h3>
-                    <h4 class="data-subtitle">
+                    <h4 class="data-card__subtitle">
                         21 3435-3534
                     </h4>
                 </div>
-                <div class="content-data">
-                    <div class="content-data-icon">
+                <div class="data-card__item">
+                    <div class="data-card__icon-card">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
-                            class="bi bi-whatsapp icon-svg" viewBox="0 0 16 16">
+                            class="bi bi-whatsapp data-card__icon" viewBox="0 0 16 16">
                             <path
                                 d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
                         </svg>
                     </div>
-                    <h3 class="data-title">
+                    <h3 class="data-card__title">
                         LEANDRO
                     </h3>
-                    <h4 class="data-subtitle">
+                    <h4 class="data-card__subtitle">
                         21 3435-3534
                     </h4>
                 </div>
-                <div class="content-data">
-                    <div class="content-data-icon">
+                <div class="data-card__item">
+                    <div class="data-card__icon-card">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
-                            class="bi bi-whatsapp icon-svg" viewBox="0 0 16 16">
+                            class="bi bi-whatsapp data-card__icon" viewBox="0 0 16 16">
                             <path
                                 d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
                         </svg>
                     </div>
-                    <h3 class="data-title">
+                    <h3 class="data-card__title">
                         SERGIO
                     </h3>
-                    <h4 class="data-subtitle">
+                    <h4 class="data-card__subtitle">
                         21 3435-3534
                     </h4>
                 </div>
-                <div class="content-data">
-                    <div class="content-data-icon">
+                <div class="data-card__item">
+                    <div class="data-card__icon-card">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
-                            class="bi bi-whatsapp icon-svg" viewBox="0 0 16 16">
+                            class="bi bi-whatsapp data-card__icon" viewBox="0 0 16 16">
                             <path
                                 d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
                         </svg>
                     </div>
-                    <h3 class="data-title">
+                    <h3 class="data-card__title">
                         RENATA
                     </h3>
-                    <h4 class="data-subtitle">
+                    <h4 class="data-card__subtitle">
                         21 3435-3534
                     </h4>
                 </div>
-                <div class="content-data">
-                    <div class="content-data-icon">
+                <div class="data-card__item">
+                    <div class="data-card__icon-card">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
-                            class="bi bi-whatsapp icon-svg" viewBox="0 0 16 16">
+                            class="bi bi-whatsapp data-card__icon" viewBox="0 0 16 16">
                             <path
                                 d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
                         </svg>
                     </div>
-                    <h3 class="data-title">
+                    <h3 class="data-card__title">
                         LUCIANA
                     </h3>
-                    <h4 class="data-subtitle">
+                    <h4 class="data-card__subtitle">
                         21 3435-3534
                     </h4>
                 </div>
-                <div class="content-data content-data-6">
-                    <div class="content-data-icon content-data-icon-6">
+                <div class="data-card__item data-card__item--wide">
+                    <div class="data-card__icon-card">
                         <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
-                            class="bi bi-geo-alt icon-svg" viewBox="0 0 16 16">
+                            class="bi bi-geo-alt data-card__icon" viewBox="0 0 16 16">
                             <path
                                 d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
                             <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
                         </svg>
                     </div>
-                    <h3 class="data-title">
+                    <h3 class="data-card__title">
                         ENDEREÇO
                     </h3>
-                    <h4 class="data-subtitle">
+                    <h4 class="data-card__subtitle">
                         RUA NIVALDO GUERREIRO NUNES, 450 <br>
                         DISTRITO INDUSTRIAL, UBERLÂNDIA MG
                     </h4>
@@ -108,17 +108,17 @@
                 Ouvidoria
             </h1>
             <form action="" class="container__form">
-                <div class="mb-3 form__form-field">
-                    <label for="input1" class="form-label form-field__label">Email address</label>
-                    <input type="email" class="form-control form-field__input" id="input1">
+                <div class="mb-3 form__field">
+                    <label for="input1" class="form-label form__label">Email address</label>
+                    <input type="email" class="form-control form__input" id="input1">
                 </div>
-                <div class="mb-3 form__form-field">
-                    <label for="input2" class="form-label form-field__label">Email address</label>
-                    <input type="email" class="form-control form-field__input" id="input2">
+                <div class="mb-3 form__field">
+                    <label for="input2" class="form-label form__label">Email address</label>
+                    <input type="email" class="form-control form__input" id="input2">
                 </div>
-                <div class="mb-3 form__form-field">
-                    <label for="textarea1" class="form-label form-field__label">Example textarea</label>
-                    <textarea class="form-control form-field__textarea" id="textarea1" rows="4"></textarea>
+                <div class="mb-3 form__field">
+                    <label for="textarea1" class="form-label form__label">Example textarea</label>
+                    <textarea class="form-control form__textarea" id="textarea1" rows="4"></textarea>
                 </div>
                 <button class="form__button">
                     ENVIAR
@@ -137,48 +137,11 @@
     background: inherit;
 }
 
-.container-data {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    width: 64%;
-    margin: 50px auto;
-    row-gap: 40px;
-    column-gap: 25%;
-}
-
-.form__form-field {
-    margin: auto;
-    width: 69%
-}
-
 .main-container__container {
     width: 50%;
     height: 100%;
     margin-bottom: 30px;
     padding-bottom: 20px
-}
-
-.container--red {
-    background-color: var(--primary-color);
-}
-
-.content-data {
-    color: white;
-    display: grid;
-    grid-template-columns: 2fr 7fr;
-}
-
-.content-data-1,
-.content-data-6 {
-    grid-template-columns: 0.8fr 7fr;
-    grid-column-start: 1;
-    grid-column-end: 3;
-}
-
-.content-data-icon {
-    grid-row-start: 1;
-    grid-row-end: 3;
-    margin-right: 12px;
 }
 
 .container__title {
@@ -188,36 +151,77 @@
     text-shadow: 0 2px 2px var(--shadow-color);
 }
 
+/* container red style */
+
+.container--red {
+    background-color: var(--primary-color);
+}
+
 .container__title--white {
     color: white;
 }
 
-.container__title--red {
-    color: var(--primary-color);
+.container__data-card {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    width: 64%;
+    margin: 50px auto;
+    row-gap: 40px;
+    column-gap: 25%;
 }
 
-.data-title {
+.data-card__item {
+    color: white;
+    display: grid;
+    grid-template-columns: 2fr 7fr;
+}
+
+.data-card__item--wide,
+.data-card__item--wide {
+    grid-template-columns: 0.8fr 7fr;
+    grid-column-start: 1;
+    grid-column-end: 3;
+}
+
+.data-card__icon-card {
+    grid-row-start: 1;
+    grid-row-end: 3;
+    margin-right: 12px;
+}
+
+.data-card__title {
     font-size: 22px;
     text-shadow: 0 1px 2px var(--shadow-color);
     padding-top: 4px;
 }
 
-.data-subtitle {
+.data-card__subtitle {
     font-size: 18px;
     font-weight: 400;
     text-shadow: 0 1px 2px var(--shadow-color);
 }
 
-.form-field__label {
+/* container white style */
+
+.container__title--red {
     color: var(--primary-color);
 }
 
-.form-field__input {
+.form__field {
+    margin: auto;
+    width: 69%
+}
+
+.form__label {
+    color: var(--primary-color);
+}
+
+.form__input {
     border-color: var(--gray);
     height: 45px;
 }
 
-.form-field__textarea {
+.form__textarea {
     border-color: var(--gray);
 }
 
@@ -241,32 +245,36 @@
         font-size: 42px;
     }
 
+    /* container red style */
+
     .container--red {
         padding: 0 10%;
     }
 
-    .container-data {
+    .container__data-card {
         column-gap: 10%;
     }
 
-    .form__form-field {
-        width: 75%
-    }
-
-    .icon-svg {
+    .data-card__icon {
         width: 28px;
         height: 28px;
     }
 
-    .data-title {
+    .data-card__title {
         font-size: 18px;
     }
 
-    .data-subtitle {
+    .data-card__subtitle {
         font-size: 14px
     }
 
-    .form-field__input {
+    /* container white style */
+
+    .form__field {
+        width: 75%
+    }
+
+    .form__input {
         height: 40px;
     }
 
@@ -279,28 +287,32 @@
 
 @media screen and (min-width: 576px) and (max-width: 767px) {
 
-    .container-data {
+    .container__title {
+        font-size: 48px;
+    }
+
+    /* container red style */
+
+    .container__data-card {
         width: 69%;
         column-gap: 22%;
     }
 
-    .icon-svg {
+    .data-card__icon {
         width: 32px;
         height: 32px;
     }
 
-    .data-title {
+    .data-card__title {
         font-size: 20px;
         padding-top: 3px;
     }
 
-    .data-subtitle {
+    .data-card__subtitle {
         font-size: 16px
     }
 
-    .container__title {
-        font-size: 48px;
-    }
+    /* container white style */
 
     .form__button {
         width: 150px;
@@ -321,8 +333,10 @@
         width: 100%;
     }
 
-    .content-data-1,
-    .content-data-6 {
+    /* container red style */
+
+    .data-card__item--wide,
+    .data-card__item--wide {
         grid-template-columns: 0.7fr 7fr;
     }
 
@@ -330,7 +344,9 @@
 
 @media screen and (min-width: 768px) and (max-width: 991px) {
 
-    .container-data {
+    /* container red style */
+
+    .container__data-card {
         width: 62%;
         column-gap: 30%;
     }
@@ -338,28 +354,32 @@
 
 @media screen and (min-width: 992px) and (max-width: 1199px) {
 
-    .container-data {
+    .container__title {
+        font-size: 52px;
+    }
+
+    /* container red style */
+
+    .container__data-card {
         width: 73%;
         column-gap: 17%;
     }
 
-    .icon-svg {
+    .data-card__icon {
         width: 32px;
         height: 32px;
     }
 
-    .data-title {
+    .data-card__title {
         font-size: 20px;
         padding-top: 3px;
     }
 
-    .data-subtitle {
+    .data-card__subtitle {
         font-size: 16px
     }
 
-    .container__title {
-        font-size: 52px;
-    }
+    /* container white style */
 
     .form__button {
         width: 150px;
@@ -370,7 +390,9 @@
 
 @media screen and (min-width: 1200px) and (max-width: 1399px) {
 
-    .container-data {
+    /* container red style */
+    
+    .container__data-card {
         width: 70%;
         column-gap: 20%;
     }
